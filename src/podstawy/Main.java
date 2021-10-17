22b62e1e16a0a88fad0b2f8ff14f4765b33f9a59
Marcin Lamek <fluffgit@gmail.com>
2021-10-17 15:14:07 +0200

    Upload

---
+++ src/podstawy/Main.java
@@ -0,0 +1,69 @@
+package podstawy;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        long start = System.nanoTime();
+        timeGen(start);
+
+
+    }
+
+    public static void timeGen(long start) throws InterruptedException {
+        boolean run = true;
+        while(run){
+            getRandomEfect(start);
+        }
+    }
+
+    public static void getRandomEfect(long start) throws InterruptedException {
+        long maximum = 900000000;
+        long minimum = 0;
+        long number = ((long) (Math.random() * (maximum - minimum))) + minimum;
+        switch ((int) number){
+            case 1:
+                System.out.println("Głodny");
+                getTimeOfEvent(start);
+                sleep();
+                break;
+            case 100:
+                System.out.println("Sprzątnij klatkę");
+                getTimeOfEvent(start);
+                sleep();
+
+                break;
+            case 10000:
+                System.out.println("Chory");
+                getTimeOfEvent(start);
+                sleep();
+
+                break;
+            case 100000:
+                System.out.println("Pobaw się");
+                getTimeOfEvent(start);
+                sleep();
+                break;
+            case 1000000:
+                System.out.println("pogłaskaj");
+                getTimeOfEvent(start);
+                sleep();
+                break;
+            case 10000000:
+                System.out.println("Szczeka");
+                getTimeOfEvent(start);
+                sleep();
+                break;
+        }
+    }
+
+    public static void sleep() throws InterruptedException {
+        Thread.sleep(2000);
+    }
+
+    public static void getTimeOfEvent(long start){
+        long elapsedTime = System.nanoTime() - start;
+        double elapsedTimeInSecond = (double) elapsedTime / 1_000_000_000;
+        System.out.println(elapsedTimeInSecond + " seconds");
+    }
+}
